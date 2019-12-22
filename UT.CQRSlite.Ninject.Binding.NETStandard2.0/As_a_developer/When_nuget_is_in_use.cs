d8dba0cde038cd107fe07faae2f489e2a39b639c
barimale <mateusz.wolnica@gmail.com>
2019-12-22 23:23:15 +0100

    UT added.

---
+++ UT.CQRSlite.Ninject.Binding.NETStandard2.0/As_a_developer/When_nuget_is_in_use.cs
@@ -0,0 +1,22 @@
+﻿using CQRSlite.Domain;
+using Ninject;
+using NUnit.Framework;
+
+namespace UT.CQRSlite.Ninject.Binding.NETCore3._0
+{
+    public class When_nuget_is_in_use
+    {
+        [Test]
+        public void I_d_like_to_have_bindings_loaded_correctly()
+        {
+            //given
+            StandardKernel kernel = new StandardKernel(new global::CQRSlite.Ninject.Binding.Bindings());
+
+            //when
+            var library = kernel.Get<ISession>();
+
+            //then
+            Assert.IsNotNull(library);
+        }
+    }
+}
