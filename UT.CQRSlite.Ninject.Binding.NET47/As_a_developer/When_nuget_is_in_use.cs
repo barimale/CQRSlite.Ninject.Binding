b1f696543a80547d30af576ada4627bb9f010a34
barimale <mateusz.wolnica@gmail.com>
2019-12-23 02:25:36 +0100

    .

---
+++ UT.CQRSlite.Ninject.Binding.NET47/As_a_developer/When_nuget_is_in_use.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using CQRSlite.Domain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ninject;
+
+namespace UT.CQRSlite.Ninject.Binding.NET47
+{
+    [TestClass]
+    public class When_nuget_is_in_use
+    {
+        [TestMethod]
+        public void I_d_like_to_have_bindings_loaded_correctly()
+        {
+            //given
+            StandardKernel kernel = new StandardKernel(new global::CQRSlite.Ninject.Binding.Bindings());
+
+            //when
+            var library = kernel.Get<ISession>();
+
+            //then
+            Assert.IsNotNull(library);
+        }
+    }
+}
---
+++ UT.CQRSlite.Ninject.Binding.NET47/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UT.CQRSlite.Ninject.Binding.NET47")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UT.CQRSlite.Ninject.Binding.NET47")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7d17a520-0ab1-47b0-8687-6b9f64d07f24")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
