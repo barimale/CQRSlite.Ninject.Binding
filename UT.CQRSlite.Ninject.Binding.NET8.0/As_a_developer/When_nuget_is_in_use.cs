6e7077d7a2e261476a953f2b52cba880b24bcac0
Mateusz <mateusz.wolnica@gmail.com>
2024-10-02 15:04:32 +0200

    .

---
+++ UT.CQRSlite.Ninject.Binding.NET8.0/As_a_developer/When_nuget_is_in_use.cs
@@ -0,0 +1,26 @@
+﻿using CQRSlite.Domain;
+using CQRSlite.Ninject.Binding;
+using Ninject;
+using NUnit.Framework;
+using UT.CQRSlite.Ninject.Binding.NETCore30.Preconfiguration;
+
+namespace UT.CQRSlite.Ninject.Binding.NETCore3._0
+{
+    public class When_nuget_is_in_use
+    {
+        [Test]
+        public void I_d_like_to_have_bindings_loaded_correctly()
+        {
+            //given
+            StandardKernel kernel = new StandardKernel(
+                new DummyBindings(),
+                new Bindings());
+
+            //when
+            var library = kernel.Get<ISession>();
+
+            //then
+            Assert.IsNotNull(library);
+        }
+    }
+}
